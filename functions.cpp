d8ad0fd46952f991e157ea617b0a2f24a5cda280
Denis Plotnikov <youngperchik2404@gmail.com>
2024-10-09 17:00:01 +0500

    new code

--- functions.cpp
+++ functions.cpp
@@ -1,45 +1,131 @@
 #include <iostream>
+#include <cstdlib>
 //функции
 
-float Sum(float num1, float num2)
-{
-	return num1 + num2;
-}
+//float Sum(float num1, float num2)
+//{
+//	return num1 + num2;
+//}
+//
+//float Subtraction(float num1, float num2)
+//{
+//	return num1 - num2;
+//}
+//
+//float Multiply(float num1, float num2)
+//{
+//	return num1 * num2;
+//}
+//
+//float Division(float num1, float num2)
+//{
+//	if (num2 == 0)
+//	{
+//		return 1;
+//	}
+//	return num1 / num2;
+//}
+//
+//float Percent(float num1, float num2)
+//{
+//	return num1 * num2 / 100;
+//}
 
-float Subtraction(float num1, float num2)
-{
-	return num1 - num2;
-}
+//void SetArray(int arr[], int size);
+//void PrintArray(int arr[], int size);
 
-float Multiply(float num1, float num2)
-{
-	return num1 * num2;
-}
+void NewArray(int arr[], int size);
+void PrintNewArray(int arr[], int size);
 
-float Division(float num1, float num2)
-{
-	if (num2 == 0)
-	{
-		return 1;
-	}
-	return num1 / num2;
-}
-
-float Percent(float num1, float num2)
-{
-	return num1 * num2 / 100;
-}
-
+void max(int  arr[], int size, int max, int maxIndex);
+void min(int  arr[], int size, int min, int minIndex);
 
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
 
-	cout << "Сумма = " << Sum(11, 9)<<endl;
+	const int size = 20;
+	int arr[size];
+	int max1 = INT_MIN, min1 = INT_MAX;
+	int minIndex{}, maxIndex{};
+
+	/*cout << "Сумма = " << Sum(11, 9)<<endl;
 	cout << "Разность = " << Subtraction(11, 9)<<endl;
 	cout << "Произведение = " << Multiply(11, 9) << endl;
 	cout << " Частное = " << Division(11, 9) << endl;
-	cout << "Процент числа = " << Percent(11, 9) << endl;
+	cout << "Процент числа = " << Percent(11, 9) << endl;*/
+	/*SetArray(arr, size);
+	PrintArray(arr, size);*/
+	NewArray(arr, size);
+	PrintNewArray(arr, size);
+	max(arr, size, max1, maxIndex);
+	min(arr, size, min1, minIndex);
+
 }
+
+void NewArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 27 - 8;
+	}
+}
+
+void PrintNewArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+void max(int arr[], int size, int max1, int maxIndex)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] > max1)
+		{
+			max1 = arr[i];
+			maxIndex = i;
+		}
+	}
+	cout << "\n\nМаксимальное число под номером "<<maxIndex<<" = " << max1;
+}
+
+void min(int arr[], int size, int min1, int minIndex)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < min1)
+		{
+			min1 = arr[i];
+			minIndex = i;
+		}
+	}
+	cout << "\nМинимальное число под номером " << minIndex << " = " << min1;
+}
+
+
+
+
+
+
+
+//void SetArray(int arr[], int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		arr[i] = rand() % 10;
+//	}
+//}
+//
+//void PrintArray(int arr[], int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout<< arr[i]<<" ";
+//	}
+//}
