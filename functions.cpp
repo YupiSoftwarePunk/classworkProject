8937cae37b4dc4aa6d72760f4a7f33234fc1f09b
Denis Plotnikov <youngperchik2404@gmail.com>
2024-10-09 14:51:11 +0500

    Добавьте файлы проекта.

---
+++ functions.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+//функции
+
+float Sum(float num1, float num2)
+{
+	return num1 + num2;
+}
+
+float Subtraction(float num1, float num2)
+{
+	return num1 - num2;
+}
+
+float Multiply(float num1, float num2)
+{
+	return num1 * num2;
+}
+
+float Division(float num1, float num2)
+{
+	return num1 / num2;
+}
+
+float Percent(float num1, float num2)
+{
+	return num1 * num2 / 100;
+}
+
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	cout << "Сумма = " << Sum(11, 9)<<endl;
+	cout << "Разность = " << Subtraction(11, 9)<<endl;
+	cout << "Произведение = " << Multiply(11, 9) << endl;
+	cout << " Частное = " << Division(11, 9) << endl;
+	cout << "Процент числа = " << Percent(11, 9) << endl;
+}
